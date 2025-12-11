937d5e5c0f3a68c0729b676829e20c7edc491d64
mariliamatosbiz <62524066+mariliamatosbiz@users.noreply.github.com>
2025-12-10 21:41:11 -0300

    TEste

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// ADICIONE ESTA LINHA AQUI:
+console.log('MINHA CHAVE:', import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
